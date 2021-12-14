fd853a6ea020f47be09231902fe0160dcd29ee94
Admin <admin@admins-macbook-pro.local>
2021-12-14 14:40:10 +0200

    Initial Commit

---
+++ edentechlabs-interview-task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  edentechlabs-interview-task
+//
+//  Created by Admin on 14.12.21.
+//  Copyright © 2021 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
